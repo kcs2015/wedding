1b190a7950d8a77604081c330176423b8529b290
kcs2015 <knightclientsolutions@gmail.com>
2016-11-03 15:30:23 -0400

    Created markup for main home page sections
    Page cover
    couple info
    Gallery Slider
    Destination
    Bridesmaids
    Groomsmen

---
+++ assets/js/jr-scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+ 
+  $("#location-carousel-main").owlCarousel({
+      autoPlay : 3000,
+    stopOnHover : true,
+    navigation:true,
+    paginationSpeed : 1000,
+    goToFirstSpeed : 2000,
+    singleItem : true,
+    transitionStyle:"fade"
+  });
+    
+ $("#couple-gallery-carousel-main").owlCarousel({
+      autoPlay : 3000,
+    stopOnHover : true,
+    navigation:true,
+    paginationSpeed : 1000,
+    goToFirstSpeed : 2000,
+    transitionStyle:"fade"
+  });
+ 
+});
