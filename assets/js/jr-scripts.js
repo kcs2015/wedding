41dea0fc8d68633be3a4790399388cf6b3a7cced
kcs2015 <knightclientsolutions@gmail.com>
2016-11-04 02:29:39 -0400

    Sticky footer, countdown, text shadows
    Added new font stylesheets
    Added section header class to headers
    Added text shadow to headers
    Added Countdown Markup
    Made Footer sticky to screen

--- assets/js/jr-scripts.js
+++ assets/js/jr-scripts.js
@@ -18,5 +18,15 @@ $(document).ready(function() {
     goToFirstSpeed : 2000,
     transitionStyle:"fade"
   });
+    
+    $('#clock').countdown('2017/11/04', function(event) {
+  var $this = $(this).html(event.strftime(''
+    + '<div id="timer-weeks" class="timer-block"> %w <br><span>Weeks</span> </div> '
+    + '<div id="timer-days" class="timer-block"> %d <br><span>Days</span> </div> '
+    + '<div id="timer-hours" class="timer-block"> %H <br><span>Hours</span> </div> '
+    + '<div id="timer-min" class="timer-block"> %M <br><span>Minutes</span> </div> '
+    + '<div id="timer-sec" class="timer-block"> %S <br><span>Seconds</span> </div>'));
+           
+});
  
 });
