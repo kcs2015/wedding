943c64a65ab9a0588e3fc30d36eb8fae4bcf1cfc
kcs2015 <knightclientsolutions@gmail.com>
2016-12-18 21:57:05 -0500

    Added royalton slider photos

--- assets/js/jr-scripts.js
+++ assets/js/jr-scripts.js
@@ -6,15 +6,7 @@ $(document).ready(function() {
 });
 
     
-    $("#location-carousel-main").owlCarousel({
-      autoPlay : 3000,
-    stopOnHover : true,
-    navigation:true,
-    paginationSpeed : 1000,
-    goToFirstSpeed : 2000,
-    singleItem : true,
-    transitionStyle:"fade"
-  });
+
     
  $(".couple-gallery-carousel").owlCarousel({
       autoPlay : 3000,
