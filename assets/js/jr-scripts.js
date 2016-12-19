57dff8e1cc70cf86779ad33b4d28266554aa3735
kcs2015 <knightclientsolutions@gmail.com>
2016-12-18 22:40:52 -0500

    Fixed no Royalton Location Carousel showing

--- assets/js/jr-scripts.js
+++ assets/js/jr-scripts.js
@@ -6,6 +6,16 @@ $(document).ready(function() {
 });
 
     
+    
+    $("#location-carousel-main").owlCarousel({
+      autoPlay : 3000,
+    stopOnHover : true,
+    navigation:true,
+    paginationSpeed : 1000,
+    goToFirstSpeed : 2000,
+    singleItem : true,
+    transitionStyle:"fade"
+  });
 
     
  $(".couple-gallery-carousel").owlCarousel({
